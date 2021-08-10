58ee9da4d4622434a3878b189f339a3b6aeba48f
Leon <hualiang.tian@gmail.com>
2021-08-10 17:25:40 -0400

    adding forms new for user and game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,32 @@
 class GamesController < ApplicationController
+    
+    def index 
+        @games = Game.all
+    end
+
+    def new
+        @game = Game.new
+    end
+
+    def create
+        # byebug
+        # binding.pry
+        @game = Game.new(game_params)
+        if @game.save
+            redirect_to game
+        else
+            render :new
+        end
+    end
+
+    def show
+        @game =  Game.find_by(params[:id])
+    end
+
+
+    private
+    
+    def game_params
+        params.require(:game).permit(:title, :categories)
+    end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,24 @@
 class SessionsController < ApplicationController
   
   def home
-
   end
   
   def new
+    @user = user.new
   end
 
   def create
+    if @user = User.find_by(username: params[:user][:username])
+      session[:user_id] = @user.id    
+      redirect_to user_path(@user)
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    session.delete("user_id")
+    redirect_to root_path
   end
 
   
