5c430010a29af4d86d9251ca2811d57803fcc82a
Leon <hualiang.tian@gmail.com>
2021-08-11 17:26:32 -0400

    ading validation for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,8 @@ class User < ApplicationRecord
     has_many :reviews  
     has_many :games, through: :reviews
 
+    validates :email, presence: true, uniqueness: true
+    validates :username, presence: true
+
     has_secure_password
 end
